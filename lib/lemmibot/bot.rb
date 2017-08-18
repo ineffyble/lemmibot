bdee169523dfc3f1941acf0219f7622f94aaad91
Effy Elden <git@effy.is>
2017-08-18 23:21:51 +1000

    Set instance variables for position X, position Y, and direction, on initialisation, create methods to expose the new variables, and add relevant test

--- lib/lemmibot/bot.rb
+++ lib/lemmibot/bot.rb
@@ -1,4 +1,18 @@
 module Lemmibot
     class Bot
+        def initialize
+            @pos_x = 0
+            @pos_y = 0
+            @direction = :north
+        end
+        def pos_x
+            return @pos_x
+        end
+        def pos_y
+            return @pos_y
+        end
+        def direction
+            return @direction
+        end
     end
 end
---
+++ spec/bot_spec.rb
@@ -0,0 +1,10 @@
+require "spec_helper"
+
+RSpec.describe Lemmibot::Bot do
+    it "starts at 0,0,NORTH" do
+        bot = Lemmibot::Bot.new
+        expect(bot.pos_x).to eq(0)
+        expect(bot.pos_y).to eq(0)
+        expect(bot.direction).to eq(:north)
+    end
+end
