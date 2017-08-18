10de324558224f14ccf49908b556a822f0de3d3b
Effy Elden <git@effy.is>
2017-08-19 02:15:38 +1000

    Add class for handling Bot commands

--- lib/lemmibot.rb
+++ lib/lemmibot.rb
@@ -1,5 +1,6 @@
 require 'lemmibot/version'
 require 'lemmibot/bot'
+require 'lemmibot/command_interface'
 
 # A toy robot simulator
 module Lemmibot
---
+++ lib/lemmibot/command_interface.rb
@@ -0,0 +1,45 @@
+module Lemmibot
+  # An interface for issuing commands to a Lemmibot via CLI/STDIN
+  class CommandInterface
+    def initialize
+      @bot = Lemmibot::Bot.new
+    end
+
+    def start
+      puts 'Lemmibot is awaiting your commands'
+      loop do
+        command = gets
+        return unless command
+        process_command(command.chomp)
+      end
+    end
+
+    def process_command(command)
+      case command.upcase
+      when /PLACE/
+        args = place_arguments(command)
+        @bot.place(args[:x], args[:y], args[:dir])
+      when /MOVE/ then @bot.move
+      when /LEFT/ then @bot.turn(:left)
+      when /RIGHT/ then @bot.turn(:right)
+      when /REPORT/ then report
+      end
+    end
+
+    # Parses arguments for a PLACE command and returns them as a hash
+    def place_arguments(command)
+      params_string = command.split(' ')[1]
+      params = params_string.split(',')
+      {
+        x: params[0].to_i,
+        y: params[1].to_i,
+        dir: params[2].downcase.to_sym
+      }
+    end
+
+    # Outputs a report of the bot's current position and direction
+    def report
+      puts "#{@bot.pos_x},#{@bot.pos_y},#{@bot.direction.upcase}"
+    end
+  end
+end
