72ee08c8ab233da7e833a214ea8e44b21fe39215
Effy Elden <git@effy.is>
2017-08-19 15:38:16 +1000

    Refactor Bot class, separating out private methods and removing unneccessary attr_readers. Refactor tests to only call public methods.

--- lib/lemmibot/bot.rb
+++ lib/lemmibot/bot.rb
@@ -1,54 +1,11 @@
 module Lemmibot
-  # A simulated toy robot on a table top
+  # A simulated toy robot on a tabletop
   class Bot
-    DIRECTIONS = %i[west north east south].freeze
-    attr_reader :pos_x
-    attr_reader :pos_y
-    attr_reader :direction
-    attr_reader :placed
-
     def initialize
-      # Set the bot's position when instantiated
       @pos_x = 0
       @pos_y = 0
       @direction = :north
       @placed = false
-    end
-
-    def valid_direction?(direction)
-      DIRECTIONS.include? direction
-    end
-
-    def set_direction(direction)
-      # Set the bot to be facing a specified valid direction
-      return false unless valid_direction? direction
-      @direction = direction
-      true
-    end
-
-    def valid_position?(position)
-      position >= 0 && position <= 4
-    end
-
-    def set_position(axis, position)
-      # Set the bot to a specified valid position on specified axis
-      return false unless valid_position? position
-      if axis == :x
-        @pos_x = position
-      else
-        @pos_y = position
-      end
-      true
-    end
-
-    def change_position(axis, value)
-      # Alter the bot's position on the table top
-      new_position = if axis == :x
-                       @pos_x + value
-                     else
-                       @pos_y + value
-                     end
-      set_position(axis, new_position)
     end
 
     def turn(relative_direction)
@@ -95,6 +52,46 @@ module Lemmibot
         y: @pos_y,
         dir: @direction
       }
+    end
+
+    private
+
+    DIRECTIONS = %i[west north east south].freeze
+
+    def valid_direction?(direction)
+      DIRECTIONS.include? direction
+    end
+
+    def set_direction(direction)
+      # Set the bot to be facing a specified valid direction
+      return false unless valid_direction? direction
+      @direction = direction
+      true
+    end
+
+    def valid_position?(position)
+      # Check if a position is on the tabletop
+      position >= 0 && position <= 4
+    end
+
+    def set_position(axis, position)
+      # Set the bot to a specified valid position on specified axis
+      return false unless valid_position? position
+      case axis
+      when :x then @pos_x = position
+      when :y then @pos_y = position
+      end
+      true
+    end
+
+    def change_position(axis, value)
+      # Alter the bot's position by given value on given axis
+      new_position = if axis == :x
+                       @pos_x + value
+                     else
+                       @pos_y + value
+                     end
+      set_position(axis, new_position)
     end
   end
 end
--- spec/bot_spec.rb
+++ spec/bot_spec.rb
@@ -1,148 +1,6 @@
 require 'spec_helper'
 
 RSpec.describe Lemmibot::Bot do
-  it 'starts at 0,0,NORTH' do
-    bot = Lemmibot::Bot.new
-    expect(bot.pos_x).to eq(0)
-    expect(bot.pos_y).to eq(0)
-    expect(bot.direction).to eq(:north)
-  end
-
-  it 'starts unplaced' do
-    bot = Lemmibot::Bot.new
-    expect(bot.placed).to eq(false)
-  end
-
-  it 'recognises valid directions' do
-    bot = Lemmibot::Bot.new
-    %i[north south east west].each do |direction|
-      expect(bot.valid_direction?(direction)).to eq(true)
-    end
-  end
-
-  it 'rejects invalid directions' do
-    bot = Lemmibot::Bot.new
-    expect(bot.valid_direction?(:cheese)).to eq(false)
-    expect(bot.valid_direction?('northwest')).to eq(false)
-    expect(bot.valid_direction?(true)).to eq(false)
-  end
-
-  it 'will face north' do
-    bot = Lemmibot::Bot.new
-    bot.set_direction(:east)
-    expect(bot.direction).to eq(:east)
-  end
-
-  it 'will not face southwest' do
-    bot = Lemmibot::Bot.new
-    bot.set_direction(:south)
-    bot.set_direction(:southwest)
-    expect(bot.direction).to eq(:south)
-  end
-
-  it 'recognises valid positions' do
-    bot = Lemmibot::Bot.new
-    (0..4).each do |position|
-      expect(bot.valid_position?(position)).to eq(true)
-    end
-  end
-
-  it 'rejects invalid positions' do
-    bot = Lemmibot::Bot.new
-    [5, 42, -10, -255].each do |position|
-      expect(bot.valid_position?(position)).to eq(false)
-    end
-  end
-
-  it 'can be placed in a valid position' do
-    bot = Lemmibot::Bot.new
-    bot.place(2, 2, :south)
-    expect(bot.placed).to eq(true)
-    expect(bot.pos_x).to eq(2)
-    expect(bot.pos_y).to eq(2)
-  end
-
-  it 'cannot be placed in an invalid position' do
-    bot = Lemmibot::Bot.new
-    bot.place(5, 2, :south)
-    expect(bot.placed).to eq(false)
-    expect(bot.pos_x).to eq(0)
-    expect(bot.pos_y).to eq(0)
-  end
-
-  it 'will not execute commands until placed' do
-    bot = Lemmibot::Bot.new
-    bot.turn(:left)
-    expect(bot.direction).to eq(:north)
-    bot.move
-    expect(bot.pos_x).to eq(0)
-    expect(bot.pos_y).to eq(0)
-  end
-
-  it 'will turn left' do
-    bot = Lemmibot::Bot.new
-    bot.place(0, 0, :north)
-    bot.turn(:left)
-    expect(bot.direction).to eq(:west)
-  end
-
-  it 'will turn right' do
-    bot = Lemmibot::Bot.new
-    bot.place(0, 0, :north)
-    bot.turn(:right)
-    expect(bot.direction).to eq(:east)
-  end
-
-  it 'will rotate 360 degrees clockwise' do
-    bot = Lemmibot::Bot.new
-    bot.place(0, 0, :north)
-    expect(bot.direction).to eq(:north)
-    bot.turn(:right)
-    expect(bot.direction).to eq(:east)
-    bot.turn(:right)
-    expect(bot.direction).to eq(:south)
-    bot.turn(:right)
-    expect(bot.direction).to eq(:west)
-    bot.turn(:right)
-    expect(bot.direction).to eq(:north)
-  end
-
-  it 'will move to the north east corner, then 2 units west' do
-    bot = Lemmibot::Bot.new
-    bot.place(0, 0, :north)
-    bot.set_direction(:north)
-    4.times do
-      bot.move
-    end
-    bot.set_direction(:east)
-    4.times do
-      bot.move
-    end
-    expect(bot.pos_x).to eq(4)
-    expect(bot.pos_y).to eq(4)
-    bot.set_direction(:west)
-    2.times do
-      bot.move
-    end
-    expect(bot.pos_x).to eq(2)
-    expect(bot.pos_y).to eq(4)
-  end
-
-  it 'will not fall off in any direction' do
-    bot = Lemmibot::Bot.new
-    bot.place(0, 0, :north)
-    %i[north south east west].each do |direction|
-      bot.set_direction(direction)
-      100.times do
-        bot.move
-      end
-      expect(bot.pos_x).to be > -1
-      expect(bot.pos_y).to be > -1
-      expect(bot.pos_x).to be < 5
-      expect(bot.pos_y).to be < 5
-    end
-  end
-
   it 'will not report until placed' do
     bot = Lemmibot::Bot.new
     expect(bot.report).to eq(false)
@@ -154,5 +12,115 @@ RSpec.describe Lemmibot::Bot do
     expect(bot.report[:x]).to eq(0)
     expect(bot.report[:y]).to eq(1)
     expect(bot.report[:dir]).to eq(:north)
+  end
+
+  it 'can be placed facing valid directions' do
+    bot = Lemmibot::Bot.new
+    %i[north south east west].each do |direction|
+      bot.place(0, 0, direction)
+      expect(bot.report[:dir]).to eq(direction)
+    end
+  end
+
+  it 'cannot be placed facing invalid directions' do
+    bot = Lemmibot::Bot.new
+    [:cheese, 'northwest', true].each do |direction|
+      bot.place(0, 0, direction)
+      # Placement will fail, so report should return false
+      expect(bot.report).to eq(false)
+    end
+  end
+
+  it 'can be placed at valid positions' do
+    bot = Lemmibot::Bot.new
+    (0..4).each do |pos_x|
+      (0..4).each do |pos_y|
+        bot.place(pos_x, pos_y, :north)
+        expect(bot.report[:x]).to eq(pos_x)
+        expect(bot.report[:y]).to eq(pos_y)
+        expect(bot.report[:dir]).to eq(:north)
+      end
+    end
+  end
+
+  it 'cannot be placed at invalid positions' do
+    bot = Lemmibot::Bot.new
+    [5, 42, -10, -255].each do |position|
+      # Test invalid positions for both X and Y axis, ensure report returns false
+      bot.place(0, position, :north)
+      expect(bot.report).to eq(false)
+      bot.place(position, 0, :north)
+      expect(bot.report).to eq(false)
+    end
+  end
+
+  it 'will not execute commands until placed' do
+    bot = Lemmibot::Bot.new
+    result = bot.turn(:left)
+    expect(result).to eq(false)
+  end
+
+  it 'will turn left' do
+    bot = Lemmibot::Bot.new
+    bot.place(0, 0, :north)
+    bot.turn(:left)
+    expect(bot.report[:dir]).to eq(:west)
+  end
+
+  it 'will turn right' do
+    bot = Lemmibot::Bot.new
+    bot.place(0, 0, :north)
+    bot.turn(:right)
+    expect(bot.report[:dir]).to eq(:east)
+  end
+
+  it 'will rotate 360 degrees clockwise' do
+    bot = Lemmibot::Bot.new
+    bot.place(0, 0, :north)
+    expect(bot.report[:dir]).to eq(:north)
+    bot.turn(:right)
+    expect(bot.report[:dir]).to eq(:east)
+    bot.turn(:right)
+    expect(bot.report[:dir]).to eq(:south)
+    bot.turn(:right)
+    expect(bot.report[:dir]).to eq(:west)
+    bot.turn(:right)
+    expect(bot.report[:dir]).to eq(:north)
+  end
+
+  it 'will move to the north east corner, then 2 units west' do
+    bot = Lemmibot::Bot.new
+    bot.place(0, 0, :north)
+    4.times do
+      bot.move
+    end
+    bot.turn(:right) # Now facing east
+    4.times do
+      bot.move
+    end
+    expect(bot.report[:x]).to eq(4)
+    expect(bot.report[:y]).to eq(4)
+    bot.turn(:right) # Now facing south
+    bot.turn(:right) # Now facing west
+    2.times do
+      bot.move
+    end
+    expect(bot.report[:x]).to eq(2)
+    expect(bot.report[:y]).to eq(4)
+  end
+
+  it 'will not fall off in any direction' do
+    bot = Lemmibot::Bot.new
+    bot.place(0, 0, :north)
+    4.times do
+      100.times do
+        bot.move
+      end
+      bot.turn(:right)
+      expect(bot.report[:x]).to be > -1
+      expect(bot.report[:y]).to be > -1
+      expect(bot.report[:x]).to be < 5
+      expect(bot.report[:y]).to be < 5
+    end
   end
 end
